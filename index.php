5b61e20a46cdbc0fd23fa85806dcd1860360101e
Valdinei Reis <valdineimtz@gmail.com>
2016-06-16 00:16:56 -0300

    Tratando com Exec

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
 try {
     $conexao = \PDO("mysql:host=localhost;dbname=pdo", "root", "root");
+
+    $query = "show tables";
+
+    $resultado = $conexao->exec($query);
+
+    print_r($resultado);
 }
 catch (\PDOException $e) {
     echo "Não foi possível estabelecer uma conexão com o banco de dados. Erro código: " . $e->getCode();

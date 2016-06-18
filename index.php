fdc31a4db6f56ab4413aa438a99f51b3ec4be2f9
Valdinei Reis <valdineimtz@gmail.com>
2016-06-18 01:39:46 -0300

    Prepare Stmt

--- index.php
+++ index.php
@@ -4,12 +4,14 @@ try {
     $conexao = \PDO("mysql:host=localhost;dbname=pdo", "root", "root");
 
     #$query = "show tables";
-    $query = "select * from clientes";
+    $query = "select * from clientes where id = :id";
 
-    foreach ($conexao->query($query) as $cliente) {
-        echo $cliente['nome'] . "<br>";
-    }
+    $stmt = $conexao->prepare($query);
+    $stmt->bindValue(":id", $_GET['id'], \PDO::PARAM_INT);
+    $stmt->execute();
 
+    print_r($resultado->fetchAll(\PDO::FETCH_ASSOC));
+    
 }
 catch (\PDOException $e) {
     echo "Não foi possível estabelecer uma conexão com o banco de dados. Erro código: " . $e->getCode();

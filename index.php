665f15f0ea5b7df78f33b872adda121b7bc94271
Valdinei Reis <valdineimtz@gmail.com>
2016-06-18 01:47:05 -0300

    Prepare Stmt

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ try {
 
     $stmt = $conexao->prepare($query);
     $stmt->bindValue(":id", $_GET['id'], \PDO::PARAM_INT);
-    $stmt->execute();
+    $resultado = $stmt->execute();
 
     print_r($resultado->fetchAll(\PDO::FETCH_ASSOC));
-    
+
 }
 catch (\PDOException $e) {
     echo "Não foi possível estabelecer uma conexão com o banco de dados. Erro código: " . $e->getCode();

90fc2da95c4fa836801c5ae904036bbe15ed9898
Valdinei Reis <valdineimtz@gmail.com>
2016-06-20 23:04:21 -0300

    Inserindo dados

--- Cliente.php
+++ Cliente.php
@@ -4,6 +4,10 @@ class Cliente
 {
     private $db;
 
+    private $id;
+    private $nome;
+    private $email;
+
     public function __construct(\PDO $db)
     {
         $this->db = $db;
@@ -16,7 +20,15 @@ class Cliente
 
     public function inserir()
     {
+        $query = "Insert into clientes(nome, email) values(:nome, :email)";
 
+        $stmt = $this->db->prepare($query);
+        $stmt->bindValue(':nome', $this->getNome());
+        $stmt->bindValue(':email', $this->getEmail());
+
+        if ($stmt->execute()) {
+            return true;
+        }
     }
 
     public function alterar()
@@ -27,5 +39,56 @@ class Cliente
     public function deletar()
     {
 
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param mixed $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
     }
 }

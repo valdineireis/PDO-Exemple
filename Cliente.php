1fc4b76a9ea1dbb12565f7a7496d7f9de2495e74
Valdinei Reis <valdineimtz@gmail.com>
2016-06-20 22:51:51 -0300

    Criando a classe Cliente

---
+++ Cliente.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cliente
+{
+    private $db;
+
+    public function __construct(\PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function listar()
+    {
+
+    }
+
+    public function inserir()
+    {
+
+    }
+
+    public function alterar()
+    {
+
+    }
+
+    public function deletar()
+    {
+
+    }
+}
